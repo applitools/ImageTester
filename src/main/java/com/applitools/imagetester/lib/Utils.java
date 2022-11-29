6bfc60dcbf450798f7be2af35aa9ca61961da6b9
levv <lev.volkovich@applitools.com>
2022-11-29 11:19:02 +0200

    Added JitPack to pom.xml to handle EyesUtilities dep.

--- src/main/java/com/applitools/imagetester/lib/Utils.java
+++ src/main/java/com/applitools/imagetester/lib/Utils.java
@@ -1,9 +1,9 @@
 package com.applitools.imagetester.lib;
 
 import com.applitools.eyes.TestResults;
-import com.applitools.commands.AnimatedDiffs;
-import com.applitools.commands.DownloadDiffs;
-import com.applitools.commands.DownloadImages;
+import com.yanirta.Commands.AnimatedDiffs;
+import com.yanirta.Commands.DownloadDiffs;
+import com.yanirta.Commands.DownloadImages;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.net.ssl.*;
