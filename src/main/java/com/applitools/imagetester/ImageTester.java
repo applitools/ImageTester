a3ce2e04355366e660a425b23ade32284d1a7e12
levv <lev.volkovich@applitools.com>
2023-03-13 12:24:28 +0200

    - Updated version number

--- src/main/java/com/applitools/imagetester/ImageTester.java
+++ src/main/java/com/applitools/imagetester/ImageTester.java
@@ -26,7 +26,7 @@ import com.applitools.imagetester.lib.TestExecutor;
 import com.applitools.imagetester.lib.Utils;
 
 public class ImageTester {
-    private static final String cur_ver = "3.3.1";
+    private static final String cur_ver = "3.3.2";
 
     public static void main(String[] args) {
 
