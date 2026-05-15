1737a228b95a226ad19a7c35e512de64f0815bbb
Valandi <valandi.allulis@gmail.com>
2026-05-15 16:48:50 -0400

    Update EyesUtilities Version

--- src/main/java/com/applitools/imagetester/ImageTester.java
+++ src/main/java/com/applitools/imagetester/ImageTester.java
@@ -26,7 +26,7 @@ import com.applitools.imagetester.lib.TestExecutor;
 import com.applitools.imagetester.lib.Utils;
 
 public class ImageTester {
-    private static final String cur_ver = "3.10.0";
+    private static final String cur_ver = "3.10.1";
     private static final String DEFAULT_THREADS = String.valueOf(Runtime.getRuntime().availableProcessors() * 2);
 
     public static void main(String[] args) {
